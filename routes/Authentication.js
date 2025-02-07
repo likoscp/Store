80baa44438da9c460a7a242fc5e193d672b60755
likoscp <Abdikhalikova_sabina@protonmail.com>
2025-02-07 17:03:36 +0500

    validation

--- routes/Authentication.js
+++ routes/Authentication.js
@@ -3,62 +3,79 @@ const router = express.Router();
 const User = require('../models/users');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const { secret }  = require('../config');
+const { secret } = require('../config');
 const middlewareAuth = require('./middlewareAuth');
 const roleMiddleware = require('./RoleMiddleware');
 const path = require('path');
+const { body, validationResult } = require('express-validator');
 
 const generateAccessToken = (id, role) => {
     const payload = {
         id,
         role
     }
-    return jwt.sign(payload, secret, {expiresIn: "24h"} )
+    return jwt.sign(payload, secret, { expiresIn: "24h" })
 }
 
-router.post('/sign-up', async (req, res) => {
-    try {
-        const { email, password, name, phone } = req.body;
-        const existingUser = await User.findOne({ email: email });
-        if ( existingUser ) {
-            return res.status(400).json({ error: "User already exist" });
+router.post('/sign-up', [
+    body('email').isEmail().withMessage('Not valid email'),
+    body('password').isLength({ min: 6 }).withMessage('Not valid password'),
+    body('name').not().isEmpty().withMessage('Not valid name'),
+    body('phone').isMobilePhone().withMessage('Not valid phone')],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
         }
-        const hashPassword = bcrypt.hashSync( password, 2 )
-        const user = new User( { email, hashPassword, name, phone } );
-        await user.save();
-        res.status(200).json( user ) ;
-    } catch (err) {
-        res.status(400).json({ error: err.message });
-    }
-});
+
+        try {
+            const { email, password, name, phone } = req.body;
+            const existingUser = await User.findOne({ email: email });
+            if (existingUser) {
+                return res.status(400).json({ error: "User already exist" });
+            }
+            const hashPassword = bcrypt.hashSync(password, 2)
+            const user = new User({ email, hashPassword, name, phone });
+            await user.save();
+            res.status(200).json(user);
+        } catch (err) {
+            res.status(400).json({ error: err.message });
+        }
+    });
 
 router.get('/sign-in', (req, res) => {
-    res.sendFile(path.join(__dirname, '../view/login.html')); 
+    res.sendFile(path.join(__dirname, '../view/login.html'));
 });
 
 router.get('/sign-up', (req, res) => {
-    res.sendFile(path.join(__dirname, '../view/register.html')); 
+    res.sendFile(path.join(__dirname, '../view/register.html'));
 });
 
-router.post('/sign-in', async (req, res) => {
-    try {
-        const { email, password } = req.body;
-        const existingUser = await User.findOne({ email: email });
-        if (!existingUser) {
-            return res.status(400).json({ error: "User doesn't exist" });
+router.post('/sign-in',
+    [
+        body('email').isEmail().withMessage('Not valid email'),
+        body('password').isLength({ min: 8 }).withMessage('Not valid password'),
+    ],
+    async (req, res) => {
+        try {
+            const { email, password } = req.body;
+            const existingUser = await User.findOne({ email: email });
+
+            if (!existingUser) {
+                return res.status(400).json({ error: "User doesn't exist" });
+            }
+            if (!existingUser.hashPassword) {
+                return res.status(400).json({ error: "Invalid user data" });
+            }
+            const isPasswordValid = bcrypt.compareSync(password, existingUser.hashPassword);
+            if (!isPasswordValid) {
+                return res.status(400).json({ error: "Password incorrect" });
+            }
+            const token = generateAccessToken(existingUser._id, existingUser.role);
+            res.json({ token });
+        } catch (err) {
+            res.status(400).json({ error: err.message });
         }
-        if (!existingUser.hashPassword) {
-            return res.status(400).json({ error: "Invalid user data" });
-        }
-        const isPasswordValid = bcrypt.compareSync(password, existingUser.hashPassword);
-        if (!isPasswordValid) {
-            return res.status(400).json({ error: "Password incorrect" });
-        }
-        const token = generateAccessToken(existingUser._id, existingUser.role);
-        res.json({ token });
-    } catch (err) {
-        res.status(400).json({ error: err.message });
-    }
-});
+    });
 
 module.exports = router;
